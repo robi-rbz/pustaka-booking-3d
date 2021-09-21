5b7bd29c344f74969d1d320c41852811aa906cf8
robi-rbz <robiaziz0110@bsi.ac.id>
2021-09-21 20:29:36 +0700

    menambahkan/install codeigniter

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+     public function index()
+     {
+          echo "<h1> MyBio </h1>";
+          echo "Nama           : Robi Aziz <br>
+          Tempat Lahir   : Sukabumi <br>
+          Tanggal Lahir  : 01 Oktober 1993";
+     }
+}
